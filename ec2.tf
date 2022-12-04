a251fcc2b03b5a9e30772f7812a1bf2330df004f
shubhch21 <shubhangich55@gmail.com>
2022-12-04 20:17:20 +0530

    Update ec2.tf
    Update instance type from t2.small to t2.micro

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_demo" {
   ami           = "ami-079b5e5b3971bd10d"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
   tags = {
     Name = "Created_By_Terraform"
   }
